3c81defd9dda67a77175e41549758436a417ab6e
Jupiter Desphy <jupiterdesphy7@gmail.com>
2022-08-15 16:15:12 -0700

    selection statements complete

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ if (input < favNumber) {
   alert ("too low");
 } else if (input > favNumber) {
   alert ("too high");
-} else {
+} else if (input == favNumber) {
   alert ("Congratulations!!!");
+}  else {
+  alert ("Please enter a valid number");
+  
 }
 
 // Exercise 2
@@ -46,7 +49,58 @@ switch (birthMonth) {
 
 // Exercise 3
 
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
 switch (typeId) {
   case "01":
-    
+    type = "Tank Top"; 
+    break;
+  case "02":
+    type = "T-Shirt";
+    break;
+  case "03":
+    type = "Long Sleeve";
+    break;
+  case "04":
+    type = "Sweat Shirt";
+    break;
+  default:
+    type = "Other";
 }
+switch (colorId) {
+  case "BL":
+    color = "Black"; 
+    break;
+  case "BU":
+    color = "Blue";
+    break;
+  case "RD":
+    color = "Red";
+    break;
+  case "PU":
+    color = "Purple";
+    break;
+  default:
+    color = "White";
+}
+
+switch (sizeId) {
+  case "S":
+    size = "Small"; 
+    break;
+  case "M":
+    size = "Medium";
+    break;
+  case "L":
+    size = "Large";
+    break;
+  case "XL":
+    size = "Extra Large";
+    break;
+  default:
+    size = "One Size Fits All";
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
